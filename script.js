aac95fc59e0eb5a6162e92cdf7270253eae061a8
Codehero <146308149+LeyuLeyuWang@users.noreply.github.com>
2024-03-11 23:11:43 -0700

    professional quality

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-window.onload = function() {
+window.onload = function () {
     var button = document.querySelector('button[aria-label="click to switch contrast mode"]');
     var header = document.querySelector('header');
     var nav = document.querySelector('nav');
     var footer = document.querySelector('footer');
 
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         document.body.classList.toggle('high-contrast');
         header.classList.toggle('high-contrast-header');
         footer.classList.toggle('high-contrast-footer');
